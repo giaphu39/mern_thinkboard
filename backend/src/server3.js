042e28ded049eed965df16b0d89cc3fc7106bc2a
giaphu39 <trangiaphu39@gmail.com>
2026-02-15 00:53:17 +0700

    merge 2 domain for deploy v2

--- backend/src/server3.js
+++ backend/src/server3.js
@@ -10,7 +10,9 @@ import dns from "node:dns/promises";
 dns.setServers(["1.1.1.1", "8.8.8.8"]); // Using Cloudflare and Google DNS
 // ... your MongoDB connection code follows
 
-
+// khi dùng chung domain 
+import path from "path";
+const __dirname = path.resolve(); // trả về đường dẫn tuyệt đối của thư mục hiện tại
 
 dotenv.config();
 // console.log(process.env.MONGO_URI); // publicize key 
@@ -18,13 +20,16 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 // connectDB();
 
+//middleware
+if(process.env.NODE_ENV !== "production"){// nghĩa là nếu 2 bên domain khác nhau thì mới chạy cors còn giống thì không cần chạy
+    //sau khi code gần xong trang homepage cần cors để cho phép kết nối backend và frontend vẫn thỏa bảo mật
+    app.use (cors({
+        origin: "http://localhost:5173",
+    }))
+    // vị trí đặt các middleware cũng rất quan trọng phải để cors này ở đầu
+    //để backend này cho phép kết nối với frontend ngay từ đầu 
+}
 
-//sau khi code gần xong trang homepage cần cors để cho phép kết nối backend và frontend vẫn thỏa bảo mật
-app.use (cors({
-    origin: "http://localhost:5173",
-}))
-// vị trí đặt các middleware cũng rất quan trọng phải để cors này ở đầu
-//để backend này cho phép kết nối với frontend ngay từ đầu 
 
 // middleware để có thể xem được định dạng req.body
 app.use(express.json()); //middleware của Express dùng để: Parse (chuyển đổi) JSON từ body của request thành object JavaScript.
@@ -52,6 +57,41 @@ app.use("/api/notes", notesRoutes);
 
 //lúc này có thể thêm vài route và viết hàm trong routes, controllers
 // app.use("/api/product", productRoutes);
+
+
+
+if(process.env.NODE_ENV === "production"){
+    // express.static là middleware để phục vụ file tĩnh (không thay đổi theo request: css,js, image, font, file build react)
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    // dist là file frontend tạo sau build tạo file gọn cho deploy
+
+    // app.get("*", (req, res) => {
+    //     res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    // }); // lấy all get request nghĩa là các file khác thì đưa nó vào index.html trong frontend
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    }); // lấy all get request 
+
+}
+// Flow:
+// Request đến Node.
+// Không match API.
+// Không match static.
+// Match app.get("*").
+// Trả index.html.
+// React load.
+// React Router đọc URL.
+// Render page.
+
+// Thứ tự thực tế
+// CORS (nếu dev)
+// express.json()
+// rateLimiter
+// custom middleware log
+// /api/notes routes
+// static frontend (production)
+// fallback "*"
+// response
 
 // trong thực tế viết vậy vì nếu DB không chạy được thì chạy app cũng chỉ tốn tài nguyên
 connectDB().then(()=>{ 
